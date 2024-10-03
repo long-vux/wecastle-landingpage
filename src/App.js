3aaaf5894eaebe32779635c6ca91e3475c57fe14
Longvu <hoanglongvu233.com>
2024-10-03 10:11:15 +0700

    Done landing page for web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header.tsx';
+import Footer from './components/Footer.tsx';
+import About from './components/About.tsx';
+import Feature from './components/Feature.tsx';
+import Hero from './components/Hero.tsx';
+import Video from './components/Video.tsx';
+import GamePlay from './components/GamePlay.tsx';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-vt323 bg-[#222] ">
+      <Header />
+      <main className=' flex flex-col gap-[100px] mt-[150px]'>
+        <Video />
+        <About />
+        <GamePlay />
+        <Hero />
+        <Feature />
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
